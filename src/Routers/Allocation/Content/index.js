29e2391e88f9f896dfe4a08e5cfda55378a5760e
Gio <1193078960@qq.com>
2018-08-23 17:30:41 +0800

    listview

--- src/Routers/Allocation/Content/index.js
+++ src/Routers/Allocation/Content/index.js
@@ -4,6 +4,7 @@ import { StickyContainer, Sticky } from 'react-sticky';
 import ReactDOM from 'react-dom';
 import './Content.css';
 
+
 function MyBody(props) {
   return (
     <div className="am-list-body my-body">
@@ -57,130 +58,144 @@ function genData(pIndex = 0) {
 
 class Content extends Component{
   constructor(props) {
-    super(props);
-    const getSectionData = (dataBlob, sectionID) => dataBlob[sectionID];
-    const getRowData = (dataBlob, sectionID, rowID) => dataBlob[rowID];
+      super(props);
+      const getSectionData = (dataBlob, sectionID) => dataBlob[sectionID];
+      const getRowData = (dataBlob, sectionID, rowID) => dataBlob[rowID];
 
-    const dataSource = new ListView.DataSource({
-      getRowData,
-      getSectionHeaderData: getSectionData,
-      rowHasChanged: (row1, row2) => row1 !== row2,
-      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
-    });
-
-    this.state = {
-      dataSource,
-      isLoading: true,
-      height: document.documentElement.clientHeight * 3 / 4,
-    };
-  }
-
-  componentDidMount() {
-    // you can scroll to the specified position
-    // setTimeout(() => this.lv.scrollTo(0, 120), 800);
-
-    const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
-    // simulate initial Ajax
-    setTimeout(() => {
-      genData();
-      this.setState({
-        dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
-        isLoading: false,
-        height: hei,
+      const dataSource = new ListView.DataSource({
+        getRowData,
+        getSectionHeaderData: getSectionData,
+        rowHasChanged: (row1, row2) => row1 !== row2,
+        sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
       });
-    }, 600);
-  }
 
-  // If you use redux, the data maybe at props, you need use `componentWillReceiveProps`
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.dataSource !== this.props.dataSource) {
-  //     this.setState({
-  //       dataSource: this.state.dataSource.cloneWithRowsAndSections(nextProps.dataSource),
-  //     });
-  //   }
-  // }
-
-  onEndReached = (event) => {
-    // load new data
-    // hasMore: from backend data, indicates whether it is the last page, here is false
-    if (this.state.isLoading && !this.state.hasMore) {
-      return;
+      this.state = {
+        dataSource,
+        isLoading: true,
+        height: document.documentElement.clientHeight * 3 / 4,
+      };
     }
-    console.log('reach end', event);
-    this.setState({ isLoading: true });
-    setTimeout(() => {
-      genData(++pageIndex);
-      this.setState({
-        dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
-        isLoading: false,
-      });
-    }, 1000);
-  }
 
-  render() {
-    const separator = (sectionID, rowID) => (
-      <div
-        key={`${sectionID}-${rowID}`}
-        style={{
-          backgroundColor: '#F5F5F9',
-          height: 8,
-          borderTop: '1px solid #ECECED',
-          borderBottom: '1px solid #ECECED',
-        }}
-      />
-    );
-    let index = data.length - 1;
-    const row = (rowData, sectionID, rowID) => {
-      if (index < 0) {
-        index = data.length - 1;
+    componentDidMount() {
+      // you can scroll to the specified position
+      // setTimeout(() => this.lv.scrollTo(0, 120), 800);
+
+      const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
+      // simulate initial Ajax
+
+
+      setTimeout(() => {
+        console.log(dataBlobs, sectionIDs, rowIDs);
+        genData();
+        if(sectionIDs.length !== rowIDs.length){
+          console.log('different');
+          sectionIDs=[];
+          rowIDs=[];
+          genData();
+        }else {
+          console.log('same');
+        }
+        console.log(dataBlobs, sectionIDs, rowIDs);
+
+
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
+          isLoading: false,
+          height: hei,
+        });
+      }, 600);
+    }
+
+    // If you use redux, the data maybe at props, you need use `componentWillReceiveProps`
+    // componentWillReceiveProps(nextProps) {
+    //   if (nextProps.dataSource !== this.props.dataSource) {
+    //     this.setState({
+    //       dataSource: this.state.dataSource.cloneWithRowsAndSections(nextProps.dataSource),
+    //     });
+    //   }
+    // }
+
+    onEndReached = (event) => {
+      // load new data
+      // hasMore: from backend data, indicates whether it is the last page, here is false
+      if (this.state.isLoading && !this.state.hasMore) {
+        return;
       }
-      const obj = data[index--];
-      return (
-        <div key={rowID} style={{ padding: '0 15px' }}>
-          <div
-            style={{
-              lineHeight: '50px',
-              color: '#888',
-              fontSize: 18,
-              borderBottom: '1px solid #F6F6F6',
-            }}
-          >{obj.title}</div>
-          <div style={{ display: '-webkit-box', display: 'flex', padding: '15px 0' }}>
-            <img style={{ height: '64px', marginRight: '15px' }} src={obj.img} alt="" />
-            <div style={{ lineHeight: 1 }}>
-              <div style={{ marginBottom: '8px', fontWeight: 'bold' }}>{obj.des}</div>
-              <div><span style={{ fontSize: '30px', color: '#FF6E27' }}>35</span>¥ {rowID}</div>
+      console.log('reach end', event);
+      this.setState({ isLoading: true });
+      setTimeout(() => {
+        genData(++pageIndex);
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
+          isLoading: false,
+        });
+      }, 1000);
+    }
+
+    render() {
+      const separator = (sectionID, rowID) => (
+        <div
+          key={`${sectionID}-${rowID}`}
+          style={{
+            backgroundColor: '#F5F5F9',
+            height: 8,
+            borderTop: '1px solid #ECECED',
+            borderBottom: '1px solid #ECECED',
+          }}
+        />
+      );
+      let index = data.length - 1;
+      const row = (rowData, sectionID, rowID) => {
+        if (index < 0) {
+          index = data.length - 1;
+        }
+        const obj = data[index--];
+        return (
+          <div key={rowID} style={{ padding: '0 15px' }}>
+            <div
+              style={{
+                lineHeight: '50px',
+                color: '#888',
+                fontSize: 18,
+                borderBottom: '1px solid #F6F6F6',
+              }}
+            >{obj.title}</div>
+            <div style={{ display: '-webkit-box', display: 'flex', padding: '15px 0' }}>
+              <img style={{ height: '64px', marginRight: '15px' }} src={obj.img} alt="" />
+              <div style={{ lineHeight: 1 }}>
+                <div style={{ marginBottom: '8px', fontWeight: 'bold' }}>{obj.des}</div>
+                <div><span style={{ fontSize: '30px', color: '#FF6E27' }}>35</span>¥ {rowID}</div>
+              </div>
             </div>
           </div>
-        </div>
-      );
-    };
+        );
+      };
 
-    return (
-      <ListView
-        ref={el => this.lv = el}
-        dataSource={this.state.dataSource}
-        renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
-          {this.state.isLoading ? 'Loading...' : 'Loaded'}
-        </div>)}
-        renderSectionHeader={sectionData => (
-          <div>{`Task ${sectionData.split(' ')[1]}`}</div>
-        )}
-        renderBodyComponent={() => <MyBody />}
-        renderRow={row}
-        renderSeparator={separator}
-        style={{
-          height: this.state.height,
-          overflow: 'auto',
-        }}
-        pageSize={4}
-        onScroll={() => { console.log('scroll'); }}
-        scrollRenderAheadDistance={500}
-        onEndReached={this.onEndReached}
-        onEndReachedThreshold={10}
-      />
-    );
-  }
+      return (
+        <ListView
+          ref={el => this.lv = el}
+          dataSource={this.state.dataSource}
+          renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+            {this.state.isLoading ? 'Loading...' : 'Loaded'}
+          </div>)}
+          renderSectionHeader={sectionData => (
+            <div>{`Task ${sectionData.split(' ')[1]}`}</div>
+          )}
+          renderBodyComponent={() => <MyBody />}
+          renderRow={row}
+          renderSeparator={separator}
+          style={{
+            height: this.state.height,
+            overflow: 'auto',
+          }}
+          pageSize={4}
+          onScroll={() => { console.log('scroll'); }}
+          scrollRenderAheadDistance={500}
+          onEndReached={this.onEndReached}
+          onEndReachedThreshold={10}
+        />
+      );
+    }
 }
 
 export default Content;
